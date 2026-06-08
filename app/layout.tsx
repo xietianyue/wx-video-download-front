084c7d59e8d4cf3dbaa3f5ab61a80237278cc0a6
xietianyue <401024421@qq.com>
2026-06-08 10:06:24 +0800

    添加google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,7 +35,22 @@ export default function RootLayout({
       lang="zh-CN"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-[#080c14]">{children}</body>
+      <body className="min-h-full flex flex-col bg-[#080c14]">
+        {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4GL746SRLS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-4GL746SRLS');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
